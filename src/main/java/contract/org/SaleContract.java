272947c8423765972cba909cc9bfd5a9ef04d7cb
mikamvra <mika.mvra@gmail.com>
2026-05-15 10:11:04 -0400

    completed the getTotalPrice abstract method

--- src/main/java/contract/org/SaleContract.java
+++ src/main/java/contract/org/SaleContract.java
@@ -5,7 +5,9 @@ import org.example.Vehicle;
 import java.math.BigDecimal;
 
 public class SaleContract extends Contract{
-    private BigDecimal salesTax =
+    private BigDecimal salesTax = new BigDecimal("0.05");
+    private BigDecimal recordingFee = new BigDecimal("100.00");
+    private boolean finance;
 
     public SaleContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, double totalPrice, double monthlyPayment) {
         super(date, customerName, customerEmail, vehicleSold, totalPrice, monthlyPayment);
@@ -14,7 +16,14 @@ public class SaleContract extends Contract{
 
     @Override
     public double getTotalPrice() {
-        return 0;
+        Vehicle v = getVehicleSold();
+        BigDecimal processingFee;
+        if (v.getPrice() < 10000){
+        processingFee = new BigDecimal("295.00");
+        } else
+            processingFee = new BigDecimal("495.00");
+        double v1 = v.getPrice() + (v.getPrice() * salesTax) + recordingFee + processingFee;
+        return v1;
     }
 
     @Override
